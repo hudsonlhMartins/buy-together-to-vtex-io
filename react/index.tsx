cb3efb41060d6d0cac95f74d44feda2b409d2252
hudson martins <hudsonlh.martins@gmail.com>
2023-10-06 14:35:57 -0300

    fix(fetch): add repeat request when change product and add title

--- react/index.tsx
+++ react/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useProduct}  from 'vtex.product-context'
+import { useProduct } from 'vtex.product-context'
 import { useCssHandles } from 'vtex.css-handles'
 
 import { useFetch } from './hooks/useFetch/useFetch'
@@ -12,9 +12,10 @@ import { BuyButton } from './components/buyButton'
 import { SkeletonBuyTogether } from './ui/skleton'
 
 
-interface IBuyTogetherProps  {
+interface IBuyTogetherProps {
   label?: string
   pixelId?: string
+  title: string
 
 }
 
@@ -26,58 +27,69 @@ const CSS_HANDLES = [
   'icon-equal',
   'title-total',
   'price-total',
-  'content-left'
+  'content-left',
+  'title-h4'
 
 ] as const
 
-function BuyTogether({label, pixelId}:IBuyTogetherProps) {
-  const {product} = useProduct() ?? {}
+function BuyTogether({ label, pixelId, title }: IBuyTogetherProps) {
+  const { product, selectedItem } = useProduct() ?? {}
   const productId = product?.productId
   const { handles } = useCssHandles(CSS_HANDLES)
 
-  const {data, loading} = useFetch<ICrossselling[]>({
-    url: `/api/catalog_system/pub/products/crossselling/similars/${productId}`
+  const { data, loading } = useFetch<ICrossselling[]>({
+    url: `/api/catalog_system/pub/products/crossselling/similars/${productId}`,
+    repeatRequest: {
+      id: selectedItem?.itemId
+    }
   })
 
   const productItems = adpterProductItem(data)
 
-  if(loading){
+  if (loading) {
     return (
-      <SkeletonBuyTogether/>
+      <SkeletonBuyTogether />
     )
   }
-  if(!data) return <></>
 
-  return(
+  if (!data || data && data.length <= 0) return <></>
 
-    <div className={`${handles['container-buy-together']} flex items-center`}>
-      <div className={`${handles['container-buy-together-products']} flex items-center`}>
-        {productItems?.map((el, index, array )=>(
-          <>
-            <div className={`${handles['wrapper-product-buy-together']}`} key={el.productId}>
-              <ProductItem item={el} />
-            </div>
-            {((index +1) < array.length) && (
+  return (
+
+    <section>
+      <div>
+        <h4 className={`${handles['title-h4']} ma0 mb4 f5 `}>{title}</h4>
+      </div>
+      <div className={`${handles['container-buy-together']} flex items-center`}>
+        <div className={`${handles['container-buy-together-products']} flex items-center`}>
+          {productItems?.map((el, index, array) => (
+            <>
+              <div className={`${handles['wrapper-product-buy-together']}`} key={el.productId}>
+                <ProductItem item={el} />
+              </div>
+              {((index + 1) < array.length) && (
                 <span className={`${handles['icon-plus']} mh5`}>
-                  <IconPlus/>
+                  <IconPlus />
                 </span>
               )}
-          </>
-        ))}
-      </div>
-      <div className={`${handles['content-left']} flex items-center`}>
-        <span className={`${handles['icon-equal']} ph5`}>
-          <IconEqual/>
-        </span>
-        <div>
+            </>
+          ))}
+        </div>
+        <div className={`${handles['content-left']} flex items-center`}>
+          <span className={`${handles['icon-equal']} ph5`}>
+            <IconEqual />
+          </span>
+          <div>
             <p className={`${handles['title-total']}`}>TOTAL COM DESCONTO:</p>
             <strong className={handles['price-total']}>
-              <TotalPrice items={productItems}/>
+              <TotalPrice items={productItems} />
             </strong>
-            <BuyButton items={data} pixelId={pixelId} label={label}/>
+            <BuyButton items={data} pixelId={pixelId} label={label} />
+          </div>
         </div>
       </div>
-    </div>
+
+    </section>
   )
 }
 
@@ -88,6 +100,10 @@ BuyTogether.schema = {
   properties: {
     label: {
       title: 'Label do botao',
+      type: 'string',
+    },
+    title: {
+      title: 'title compre junto',
       type: 'string',
     },
     pixelId: {
